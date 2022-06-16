56838203a9f89f1dcc07ccfd6d8ced6dac1bd4e5
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-06-16 20:35:29 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+
+function saturdayFun(toDo = 'roller-skate') {
+ return (`This Saturday, I want to ${toDo}!`);
+}
+
+saturdayFun();
+
+
+function mondayWork(activity = 'go to the office') {
+   return (`This Monday, I will ${activity}.`);
+}
+
+
+function wrapAdjective(init = "*") {
+    return function(wrap = "special") {
+        return (`You are ${init}${wrap}${init}!`);
+
+    }
+}
